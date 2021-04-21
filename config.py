55fdc509b7039d198fd38b67679a2402f6c3b410
Turt-le-go <musijivan27@gmail.com>
2021-04-21 09:04:04 +0300

    del *

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ FILE_SUFFIX = ("work","home","todo")
 
 
 HELP_MESSAGE = """
-*    --help - вивести це повідомлення
+    --help - вивести це повідомлення
 
     notes *args - зберегти args в сьогоднішню нотатку
-*    notes work,todo,home *args - зберегти args в нотатку з таким суфіксом (суфікси можна змінити в config.py)
-*    notes init - створити папку (і репозиторій) для нотаток
+    notes work,todo,home *args - зберегти args в нотатку з таким суфіксом (суфікси можна змінити в config.py)
+    notes init - створити папку (і репозиторій) для нотаток
     notes push *args - зберегти нотатки в github
     notes pull - стягнути всі нотатки з github
     notes - відкрити нотатку за сьогодні
